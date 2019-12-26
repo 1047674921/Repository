deae8c30d94f985b1b81ae1db31677d142300577
GREEY Roc <lianshan4921@foxmail.com>
2019-12-26 23:03:00 +0800

    百钱买百鸡

---
+++ DataStructuresAndAlgorithm/Chicken/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Chicken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 3; i < 100; i += 3)
+            {
+                for (j = 1; j < 100; j++)
+                {
+                    if (i / 3 + j * 3 + (100 - i - j) * 5 == 100 && 100 - i - j > 0)
+                    {
+                        Console.WriteLine("小鸡 " + i + "\t母鸡 " + j + "\t公鸡 " + (100 - i - j));
+                    }
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
